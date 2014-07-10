16c8a9a8187df7755babea36ef31f96729a8a5d3
mathieu-pousse <mathieu.pousse@zenika.com>
2014-07-10 22:25:25 +0200

    make the application configurable through system properties

--- src/main/java/org/iwb/site/ApplicationConfiguration.java
+++ src/main/java/org/iwb/site/ApplicationConfiguration.java
@@ -1,10 +1,10 @@
 package org.iwb.site;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 /**
  * TODO document me
@@ -24,9 +25,25 @@ import java.net.UnknownHostException;
 @ImportResource("classpath:spring-bootstrap.xml")
 public class ApplicationConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfiguration.class);
+
     @Bean
     public Jongo jongo() throws UnknownHostException {
-        DB db = new MongoClient("localhost").getDB("iwb-web");
+        String host = System.getProperty("mongodb.host", "localhost");
+        int port = Integer.valueOf(System.getProperty("mongodb.port", "27017"));
+        String user = System.getProperty("monngodb.user");
+        String pass = System.getProperty("monngodb.pass");
+        String database = System.getProperty("monngodb.db", "iwb-dev");
+
+        DB db;
+        if (user != null) {
+            db = new MongoClient(new ServerAddress(host, port),
+                    Arrays.asList(MongoCredential.createPlainCredential(user, database, pass.toCharArray())))
+                    .getDB(database);
+        } else {
+            LOGGER.warn("connecting to mongodb w/o credentials");
+            db = new MongoClient(new ServerAddress(host, port)).getDB(database);
+        }
         return new Jongo(db);
     }
 
